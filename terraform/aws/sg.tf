537afeff9f66ce2647e25ec1494bfa5dddeac590
arielkru <63583491+arielkru@users.noreply.github.com>
2021-03-04 17:37:01 +0200

    Update sg.tf

--- terraform/aws/sg.tf
+++ terraform/aws/sg.tf
@@ -1,3 +1,16 @@
+resource "aws_api_gateway_method" "any" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
+  http_method   = "ANY"
+  authorization = "NONE"
+  authorizer_id = aws_api_gateway_authorizer.this.id
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+
 resource "aws_security_group" "bar-sg" {
   name   = "sg-bar"
   vpc_id = aws_vpc.main.id
@@ -15,6 +28,7 @@ resource "aws_security_group" "bar-sg" {
     to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "bar"
   }
   
   tags = {
@@ -23,14 +37,43 @@ resource "aws_security_group" "bar-sg" {
   }
 }  
 
-resource "aws_api_gateway_method" "any" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.this.id
-  http_method   = "ANY"
-  authorization = "NONE"
-  authorizer_id = aws_api_gateway_authorizer.this.id
+resource "aws_security_group" "bar-sg" {
+  name   = "sg-bar"
+  vpc_id = aws_vpc.main.id
 
-  request_parameters = {
-    "method.request.path.proxy" = true
+  ingress {
+    from_port = 3389
+    to_port   = 3389
+    protocol  = "tcp"
+    cidr_blocks = ["192.168.0.0/16", "0.0.0.0/0"]
+    description = "foo"
   }
-}
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "bar"
+  }
+  
+  tags = {
+    Name = "allow_tls"
+    project = "open_to_hack"
+  }
+}  
+
+resource "aws_s3_bucket" "data" {
+  # Test
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+
