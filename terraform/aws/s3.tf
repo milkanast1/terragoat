064391026b3d2c2116203f51a3f2b4863749cbf6
arielkru <63583491+arielkru@users.noreply.github.com>
2021-03-04 17:00:32 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -43,6 +43,7 @@ resource "aws_s3_bucket" "financials" {
 resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
+  
   bucket = "${local.resource_prefix.value}-operations"
   acl    = "private"
   versioning {
