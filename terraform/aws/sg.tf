fa5f97563f0da75f321558ef13326cb980070066
arielkru <63583491+arielkru@users.noreply.github.com>
2021-03-04 17:43:24 +0200

    Update sg.tf

--- terraform/aws/sg.tf
+++ terraform/aws/sg.tf
@@ -77,3 +77,43 @@ resource "aws_s3_bucket" "misconfigured" {
     Environment = local.resource_prefix.value
   }
  }
+
+resource "aws_s3_bucket_object" "data_object" {
+  bucket = aws_s3_bucket.data.id
+  key    = "customer-master.xlsx"
+  source = "resources/customer-master.xlsx"
+  tags = {
+    Name        = "${local.resource_prefix.value}-customer-master"
+    Environment = local.resource_prefix.value
+  }
+}
+
+resource "aws_s3_bucket" "financials" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-financials"
+    Environment = local.resource_prefix.value
+  }
+
+}
+
+resource "aws_s3_bucket" "operations" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  bucket = "${local.resource_prefix.value}-operations"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-operations"
+    Environment = local.resource_prefix.value
+  }
+
+}
