a012bcfa16b554ce9e4f0f17b826b01b78703c18
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-15 12:45:45 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,11 +1,14 @@
 resource "aws_s3_bucket" "data" {
+  
   # Test
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
+  
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
@@ -15,9 +18,11 @@ resource "aws_s3_bucket" "data" {
 }
 
 resource "aws_s3_bucket_object" "data_object" {
+  
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
+  
   tags = {
     Name        = "${local.resource_prefix.value}-customer-master"
     Environment = local.resource_prefix.value
@@ -25,6 +30,7 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
+  
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
@@ -40,10 +46,12 @@ resource "aws_s3_bucket" "financials" {
 
 resource "aws_s3_bucket" "operations" {
   
+  
   # bucket is not encrypted
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
   acl    = "private"
+  
   versioning {
     enabled = true
   }
@@ -56,6 +64,7 @@ resource "aws_s3_bucket" "operations" {
 }
 
 resource "aws_s3_bucket" "data_science" {
+  
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
@@ -63,6 +72,7 @@ resource "aws_s3_bucket" "data_science" {
     enabled = true
   }
   logging {
+    
     target_bucket = "${aws_s3_bucket.logs.id}"
     target_prefix = "log/"
   }
@@ -75,6 +85,7 @@ resource "aws_s3_bucket" "logs" {
   versioning {
     enabled = true
   }
+  
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -83,6 +94,7 @@ resource "aws_s3_bucket" "logs" {
       }
     }
   }
+  
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-logs"
