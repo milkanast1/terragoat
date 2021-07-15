676935d7c562caed3c5333596433f06e2d79fae6
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-15 13:17:52 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -5,8 +5,11 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
+  
   acl           = "public-read"
+  
   force_destroy = true
+  
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
@@ -29,8 +32,10 @@ resource "aws_s3_bucket" "financials" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
-  acl           = "private"
-  force_destroy = true
+  
+  acl           = "public"
+  
+  force_destroy = false
   tags = {
     Name        = "${local.resource_prefix.value}-financials"
     Environment = local.resource_prefix.value
@@ -43,11 +48,14 @@ resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
+  
   acl    = "private"
+  
   versioning {
-    enabled = true
+    enabled = false
   }
   force_destroy = true
+  
   tags = {
     Name        = "${local.resource_prefix.value}-operations"
     Environment = local.resource_prefix.value
@@ -60,9 +68,11 @@ resource "aws_s3_bucket" "data_science" {
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
+    
     enabled = true
   }
   logging {
+    
     target_bucket = "${aws_s3_bucket.logs.id}"
     target_prefix = "log/"
   }
@@ -73,13 +83,16 @@ resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
   versioning {
+    
     enabled = true
   }
   server_side_encryption_configuration {
+    
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+        
+        sse_algorithm     = "aaa"
+        kms_master_key_id = "aaa"
       }
     }
   }
