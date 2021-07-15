1e7329947b1e619911164a3a74aa70a90c274225
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-15 13:05:11 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,10 +1,12 @@
 resource "aws_s3_bucket" "data" {
+  
   # Test
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
+  
   acl           = "public-read"
   force_destroy = true
   tags = {
@@ -15,6 +17,7 @@ resource "aws_s3_bucket" "data" {
 }
 
 resource "aws_s3_bucket_object" "data_object" {
+  
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
@@ -56,11 +59,13 @@ resource "aws_s3_bucket" "operations" {
 }
 
 resource "aws_s3_bucket" "data_science" {
+  
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
     enabled = true
+    
   }
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
@@ -70,12 +75,15 @@ resource "aws_s3_bucket" "data_science" {
 }
 
 resource "aws_s3_bucket" "logs" {
+  
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
   versioning {
     enabled = true
+    
   }
   server_side_encryption_configuration {
+    
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm     = "aws:kms"
@@ -83,6 +91,7 @@ resource "aws_s3_bucket" "logs" {
       }
     }
   }
+  
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-logs"
