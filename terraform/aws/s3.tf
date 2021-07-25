c83203cf56e77d31897bcd0741917ada7c09bd1e
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-25 14:39:05 +0300

    Update terraform/aws/s3.tf
    Co-authored-by: bridgecrew-staging[bot] <83755608+bridgecrew-staging[bot]@users.noreply.github.com>

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -101,6 +101,7 @@ resource "aws_s3_bucket" "logs" {
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
+  sse_algorithm = "AES256"
 }
 
 resource "aws_instance" "test" {
