728cd29f9e6861f396232adf9bed7393cb368805
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-26 18:29:07 +0300

    Update main.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ resource "aws_s3_bucket" "data" {
     }
   }
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
