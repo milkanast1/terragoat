eb94f73c93d1fa8e4cb00f8ed832a9ecc7c11ca9
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-26 18:28:35 +0300

    Update main.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ resource "aws_ebs_volume" "web_host_storage" {
 }
 
 resource "aws_s3_bucket" "data" {
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
