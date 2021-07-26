ed362e4a28fa1c2d5432b6961a4ca7b1cdc9f980
arielkru <63583491+arielkru@users.noreply.github.com>
2021-07-26 12:02:41 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -9,6 +9,7 @@ resource "aws_s3_bucket" "data" {
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
+    
     Environment = local.resource_prefix.value
     Test        = "This is a TFC test"
   }
@@ -18,9 +19,11 @@ resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
+  
   tags = {
     Name        = "${local.resource_prefix.value}-customer-master"
     Environment = local.resource_prefix.value
+    
   }
 }
 
@@ -69,6 +72,7 @@ resource "aws_s3_bucket" "data_science" {
   force_destroy = true
 }
 
+
 resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
@@ -77,9 +81,11 @@ resource "aws_s3_bucket" "logs" {
   }
   server_side_encryption_configuration {
     rule {
+      
       apply_server_side_encryption_by_default {
         sse_algorithm     = "aws:kms"
         kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+        
       }
     }
   }
