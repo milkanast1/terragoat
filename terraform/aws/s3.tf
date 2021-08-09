98c932c481f0a7a9951b95e8603cb275f5460811
arielkru <63583491+arielkru@users.noreply.github.com>
2021-08-09 14:42:37 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket" "data" {
   # Test
   # bucket is public
   # bucket is not encrypted
+  
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
@@ -12,6 +13,7 @@ resource "aws_s3_bucket" "data" {
     Environment = local.resource_prefix.value
     Test        = "This is a TFC test"
   }
+  
 }
 
 resource "aws_s3_bucket_object" "data_object" {
@@ -19,6 +21,7 @@ resource "aws_s3_bucket_object" "data_object" {
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
   tags = {
+    
     Name        = "${local.resource_prefix.value}-customer-master"
     Environment = local.resource_prefix.value
   }
@@ -27,6 +30,7 @@ resource "aws_s3_bucket_object" "data_object" {
 resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
+  
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
@@ -41,6 +45,7 @@ resource "aws_s3_bucket" "financials" {
 resource "aws_s3_bucket" "operations" {
   
   # bucket is not encrypted
+  
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
   acl    = "private"
@@ -67,17 +72,20 @@ resource "aws_s3_bucket" "data_science" {
     target_prefix = "log/"
   }
   force_destroy = true
+  
 }
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
   versioning {
+    
     enabled = true
   }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
+        
         sse_algorithm     = "aws:kms"
         kms_master_key_id = "${aws_kms_key.logs_key.arn}"
       }
@@ -86,6 +94,7 @@ resource "aws_s3_bucket" "logs" {
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-logs"
+    
     Environment = local.resource_prefix.value
   }
 }
