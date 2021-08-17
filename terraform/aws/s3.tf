3a9b8d6c005336ce6a8937111e49b2fe7f7966ec
arielkru <63583491+arielkru@users.noreply.github.com>
2021-08-17 16:19:26 +0300

    Create s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "data" {
+  
   # Test
   # bucket is public
   # bucket is not encrypted
@@ -15,6 +16,7 @@ resource "aws_s3_bucket" "data" {
 }
 
 resource "aws_s3_bucket_object" "data_object" {
+  
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
@@ -25,6 +27,7 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
+  
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
@@ -37,6 +40,7 @@ resource "aws_s3_bucket" "financials" {
   }
 
 }
+
 
 resource "aws_s3_bucket" "operations" {
   
@@ -56,6 +60,7 @@ resource "aws_s3_bucket" "operations" {
 }
 
 resource "aws_s3_bucket" "data_science" {
+  
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
@@ -84,6 +89,7 @@ resource "aws_s3_bucket" "logs" {
     }
   }
   force_destroy = true
+  
   tags = {
     Name        = "${local.resource_prefix.value}-logs"
     Environment = local.resource_prefix.value
