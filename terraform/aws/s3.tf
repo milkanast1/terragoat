3e806070a707bcc7244466e03d00d2966b385f34
arielkru <63583491+arielkru@users.noreply.github.com>
2021-08-18 18:18:25 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "data" {
+ 
   # Test
   # bucket is public
   # bucket is not encrypted
@@ -25,6 +26,7 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
+  
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
@@ -56,6 +58,7 @@ resource "aws_s3_bucket" "operations" {
 }
 
 resource "aws_s3_bucket" "data_science" {
+  
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
@@ -76,6 +79,7 @@ resource "aws_s3_bucket" "logs" {
     enabled = true
   }
   server_side_encryption_configuration {
+    
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm     = "aws:kms"
@@ -87,5 +91,81 @@ resource "aws_s3_bucket" "logs" {
   tags = {
     Name        = "${local.resource_prefix.value}-logs"
     Environment = local.resource_prefix.value
+    
+  }
+}
+
+module "bridgecrew-read" {
+  source                     = "bridgecrewio/bridgecrew-azure-read-only/azurerm"
+  org_name               = "geisenkot"
+  bridgecrew_token = "c55fa0df-5cd5-52df-b8d1-25a8a57adb31"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_postgresql_server" "postgress" {
+  name                = "psqlserver"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  administrator_login          = "psqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+
+  sku_name   = "GP_Gen5_4"
+  version    = "9.6"
+  storage_mb = 640000
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = true
+  auto_grow_enabled            = true
+
+  public_network_access_enabled    = false
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "guy-test-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+
+  tags = {
+    drift = "no"
+    yor_trace = "4afc55d6-28f0-4a20-84ea-ababeadfd2b0"
   }
 }
