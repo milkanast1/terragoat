07ddaefdd411e9e69c007cb09e2122ff955d8c3b
arielkru <63583491+arielkru@users.noreply.github.com>
2021-09-29 14:40:34 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -132,21 +132,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
- ## Line added
- ## Line added
- ## Line added
- ## Line added - below resource was in line 146
+#10 lines were deleted 
 resource "azurerm_app_service" "example" {
   name                = "guy-test-app-service"
   location            = azurerm_resource_group.example.location
