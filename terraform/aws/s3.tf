dee217cebe52e256474867c4062437e558b6539f
arielkru <63583491+arielkru@users.noreply.github.com>
2021-09-29 18:18:16 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -145,6 +145,9 @@ resource "azurerm_postgresql_server" "postgress" {
   public_network_access_enabled    = false
   ssl_enforcement_enabled          = true
   ssl_minimal_tls_version_enforced = "TLS1_2"
+  #added_line
+  #added_line
+  #added_line
 }
 
 resource "azurerm_resource_group" "example" {
