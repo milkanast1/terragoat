e60a9929fb3a19fb6591d07626ed5e0443e75e6f
arielkru <63583491+arielkru@users.noreply.github.com>
2021-10-27 18:27:19 +0300

    Fix BC_AWS_S3_14
    test description
    
    Co-authored-by: bridgecrew-dev[bot] <83754225+bridgecrew-dev[bot]@users.noreply.github.com>

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -101,4 +101,11 @@ resource "aws_s3_bucket" "test" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
