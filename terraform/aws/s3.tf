10425f2cc6dcbb7b7fb644b3574ccdf7682f646c
arielkru <63583491+arielkru@users.noreply.github.com>
2021-10-28 10:38:37 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -92,3 +92,98 @@ resource "aws_s3_bucket" "logs" {
 
 resource "aws_s3_bucket" "firstRun" {
 }
+
+
+#######################
+
+resource "aws_s3_bucket" "data2" {
+  # Test
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+    Test        = "This is a TFC test"
+  }
+}
+
+resource "aws_s3_bucket_object" "data_object2" {
+  bucket = aws_s3_bucket.data.id
+  key    = "customer-master.xlsx"
+  source = "resources/customer-master.xlsx"
+  tags = {
+    Name        = "${local.resource_prefix.value}-customer-master"
+    Environment = local.resource_prefix.value
+  }
+}
+
+resource "aws_s3_bucket" "financials2" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-financials"
+    Environment = local.resource_prefix.value
+  }
+
+}
+
+resource "aws_s3_bucket" "operations2" {
+  
+  # bucket is not encrypted
+  # bucket does not have access logs
+  bucket = "${local.resource_prefix.value}-operations"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-operations"
+    Environment = local.resource_prefix.value
+  }
+
+}
+
+resource "aws_s3_bucket" "data_science2" {
+  # bucket is not encrypted
+  bucket = "${local.resource_prefix.value}-data-science"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "log/"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "logs2" {
+  bucket = "${local.resource_prefix.value}-logs"
+  acl    = "log-delivery-write"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-logs"
+    Environment = local.resource_prefix.value
+  }
+}
