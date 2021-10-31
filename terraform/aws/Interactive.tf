eda9a79206add796f3c69e3d411a77b4f3a22442
arielkru <63583491+arielkru@users.noreply.github.com>
2021-10-31 13:01:48 +0200

    Fix 2 and Create 7 errors

--- terraform/aws/Interactive.tf
+++ terraform/aws/Interactive.tf
@@ -2,4 +2,15 @@ resource "aws_s3_bucket" "one" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "two" {
 }
