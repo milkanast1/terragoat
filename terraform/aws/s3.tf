04433a13674a528306e1184d6a15281c71ed46e0
arielkru <63583491+arielkru@users.noreply.github.com>
2021-10-31 12:54:11 +0200

    Fix 2 and create several

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "data" {
+  
   # Test
   # bucket is public
   # bucket is not encrypted
@@ -94,4 +95,14 @@ resource "aws_s3_bucket" "test1" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
+  }
 }
+
+
