a380c8b34d58835d9507f94270708b00622ed16c
arielkru <63583491+arielkru@users.noreply.github.com>
2021-10-31 13:07:42 +0200

    Fix 2 errors and create 7

--- Interactive.tf
+++ Interactive.tf
@@ -1,5 +1,20 @@
+resource "aws_s3_bucket" "resource2" {
+  # 7 new errors
+}
+
+
+
 resource "aws_s3_bucket" "resource1" {
+  # Fix 2 errors
   versioning {
     enabled = true
+  }
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
   }
 }
